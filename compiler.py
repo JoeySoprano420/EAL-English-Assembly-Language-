4039e2c17b609b5fb37396f272476715d5409ff0
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-08-22 15:06:54 -0400

    Create compiler.py

---
+++ compiler.py
@@ -0,0 +1,8 @@
+# eal/compiler.py
+class EALCompiler:
+    def __init__(self, code):
+        self.code = code
+
+    def compile(self):
+        # Logic to compile the code into bytecode or another intermediate form
+        pass
